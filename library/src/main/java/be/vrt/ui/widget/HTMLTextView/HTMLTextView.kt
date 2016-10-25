435d7ad01675f39baa5c572931f7c9bb92834438
Tim Rijckaert <tim.rijckaert@vrt.be>
2016-10-26 00:27:52 +0200

    To discuss.

---
+++ library/src/main/java/be/vrt/ui/widget/HTMLTextView/HTMLTextView.kt
@@ -0,0 +1,14 @@
+package be.vrt.ui.widget.HTMLTextView
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+
+class HTMLTextView : TextView {
+
+    constructor(context: Context, attr: AttributeSet) : this(context, attr, 0)
+
+    constructor(context: Context, attr: AttributeSet, defStyle: Int) : super(context, attr, defStyle) {
+
+    }
+}
