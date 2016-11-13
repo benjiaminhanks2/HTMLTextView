267d1fecf9af6827a38a4baa868d87b715302bd2
Tim Rijckaert <tim.rijckaert@vrt.be>
2016-11-13 18:02:39 +0100

    A User delegate example

--- app/src/main/kotlin/be/vrt/ui/sample/MainActivity.kt
+++ app/src/main/kotlin/be/vrt/ui/sample/MainActivity.kt
@@ -1,11 +1,20 @@
 package be.vrt.ui.sample
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import be.vrt.ui.HTMLTextView
+import be.vrt.ui.adapter.delegate.AbsHTMLElementAdapterDelegate
+import be.vrt.ui.model.HTMLElement
+import be.vrt.ui.model.type.HTMLTag
 import be.vrt.ui.utils.htmlTextView
-import org.jetbrains.anko.matchParent
-import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +30,47 @@ class MainActivity : AppCompatActivity() {
         }
 
         htmlTextView?.setHTMLString(this.getString(R.string.example_spv_html))
-        //htmlTextView?.addAdapterDelegate()
+        htmlTextView?.addAdapterDelegate(QuoteDelegate() as AbsHTMLElementAdapterDelegate<RecyclerView.ViewHolder>)
+    }
+
+    class QuoteDelegate : AbsHTMLElementAdapterDelegate<QuoteViewHolder>() {
+        override val viewType: Int
+            get() = 34355
+
+        override fun onBindViewHolder(item: HTMLElement, viewHolder: QuoteViewHolder, payLoads: List<Any>) = viewHolder.bind(item)
+
+        override fun isForViewType(item: HTMLElement, items: List<HTMLElement>, position: Int): Boolean {
+            var isForViewType = false
+            if (item.htmlTag != null && item.cssClass != null) {
+                val cssClass = item.cssClass
+                if (cssClass?.size ?: 0 >= 0) {
+                    val isQuote = cssClass?.any { it == "quote" } == true
+                    isForViewType = item.htmlTag == HTMLTag.P && isQuote
+                }
+            }
+            return isForViewType
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup): QuoteViewHolder {
+            val itemView = parent.context.UI {
+                textView {
+                    id = 0
+                    textColor = Color.RED
+                    setTypeface(typeface, Typeface.ITALIC)
+                    setLineSpacing(1.5F, 2F)
+                    textSize = 15f
+                    padding = dip(8)
+                    gravity = Gravity.CENTER_HORIZONTAL
+                }
+            }.view
+            return QuoteViewHolder(itemView)
+        }
+    }
+
+    class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: HTMLElement) {
+            val textView = itemView.findViewById(0) as TextView
+            textView.text = "Quote: ${item.text}"
+        }
     }
 }
--- library/src/main/kotlin/be/vrt/ui/adapter/delegate/AbsHTMLElementAdapterDelegate.kt
+++ library/src/main/kotlin/be/vrt/ui/adapter/delegate/AbsHTMLElementAdapterDelegate.kt
@@ -4,24 +4,24 @@ import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import be.vrt.ui.model.HTMLElement
 
-abstract class AbsHTMLElementAdapterDelegate<VH : RecyclerView.ViewHolder> {
+open abstract class AbsHTMLElementAdapterDelegate<VH : RecyclerView.ViewHolder> {
 
     abstract val viewType: Int
 
-    internal fun onFailedToRecycleView(holder: RecyclerView.ViewHolder): Boolean = false
+    open fun onFailedToRecycleView(holder: RecyclerView.ViewHolder): Boolean = false
 
-    internal abstract fun onBindViewHolder(item: HTMLElement, viewHolder: VH, payLoads: List<Any>)
+    abstract fun onBindViewHolder(item: HTMLElement, viewHolder: VH, payLoads: List<Any>)
 
-    internal abstract fun isForViewType(item: HTMLElement, items: List<HTMLElement>, position: Int): Boolean
+    abstract fun isForViewType(item: HTMLElement, items: List<HTMLElement>, position: Int): Boolean
 
-    internal abstract fun onCreateViewHolder(parent: ViewGroup): VH
+    abstract fun onCreateViewHolder(parent: ViewGroup): VH
 
-    internal fun onViewRecycled(holder: RecyclerView.ViewHolder) {
+    open fun onViewRecycled(holder: RecyclerView.ViewHolder) {
     }
 
-    internal fun onViewAttachedToWindow(holder: RecyclerView.ViewHolder) {
+    open fun onViewAttachedToWindow(holder: RecyclerView.ViewHolder) {
     }
 
-    internal fun onViewDetachedFromWindow(holder: RecyclerView.ViewHolder) {
+    open fun onViewDetachedFromWindow(holder: RecyclerView.ViewHolder) {
     }
 }
