cf08e62e82044993ba34aa9784a4a41c78ac4912
Tim Rijckaert <tim.rijckaert@vrt.be>
2017-01-21 22:17:20 +0100

    Upgraded the dependencies

--- app/src/main/kotlin/be/vrt/ui/sample/MainActivity.kt
+++ app/src/main/kotlin/be/vrt/ui/sample/MainActivity.kt
@@ -2,7 +2,6 @@ package be.vrt.ui.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.app.AppCompatDelegate
 import android.support.v7.widget.RecyclerView
 import be.vrt.ui.HTMLTextView
 import be.vrt.ui.adapter.delegate.AbsHTMLElementAdapterDelegate
@@ -16,7 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     var htmlTextView: HTMLTextView? = null
 
-    //TODO Show speed comparison between WebView (let's hope it's faster)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,8 +22,6 @@ class MainActivity : AppCompatActivity() {
             htmlTextView = htmlTextView {
             }.lparams(width = matchParent, height = matchParent)
         }
-
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
         htmlTextView?.setHTMLString(this.getString(R.string.example_html))
         htmlTextView?.addAdapterDelegate(QuoteDelegate() as AbsHTMLElementAdapterDelegate<RecyclerView.ViewHolder>)
